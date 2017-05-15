9dc69c8614e1e8f2b26aeaa165aeec0138fbd525
Lubos Michalic <lnmichalic@gmail.com>
2017-05-15 15:56:39 +0100

    Write Feature test to have a maximum topup value

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -1,17 +1,22 @@
 require 'oystercard'
 
-describe Oystercard do
-  # In order to use public transport
-  # As a customer
-  # I want money on my card
-  it "Has a balance" do
-    expect(subject.balance).to eq 0
-  end
-
-  # In order to keep using public transport
-  # As a customer
-  # I want to add money to my card
-  it "Allows balance to be topped up" do
-    expect(subject.topup(10)).to eq 10
+describe 'Feature Tests' do
+  describe Oystercard do
+    let(:card) { Oystercard.new }
+    # In order to use public transport
+    # As a customer
+    # I want money on my card
+    it 'Has a balance' do
+      expect(card.balance).to eq 0
+    end
+    # In order to keep using public transport
+    # As a customer
+    # I want to add money to my card
+    it 'Allows balance to be topped up' do
+      expect(card.topup(10)).to eq 10
+    end
+    it 'Prevents balance exceeding £90' do
+      expect { card.topup(95) }.to raise_error('Cannot top up at limit')
+    end
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
-
   it'allows an Oystercard to have a balance' do
     expect(subject.balance).to eq 0
   end
 
-  it "It responds to topup" do
+  it 'It responds to topup' do
     expect(subject).to respond_to(:topup).with(1).argument
   end
 
-  it "Topup adds money to balance" do
+  it 'Topup adds money to balance' do
     subject.topup(15)
     expect(subject.balance).to eq 15
   end
+
 end
