9cccf2a67a26829139c8b9b7dadff0ca481a8741
Lubos Michalic <lnmichalic@gmail.com>
2017-05-15 17:55:49 +0100

    Fixed merge conflicts that arose in last commit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,11 @@
 require_relative './station'
+
 class Oystercard
 
   MAX_BALANCE = 90
   MINIMUM_FARE = 1
 
-
   attr_reader :balance, :in_use, :journey_log
-
-  attr_reader :balance
-  attr_reader :in_use
-
 
   def initialize(balance = 0, max_balance = MAX_BALANCE)
     @in_use = false
@@ -25,38 +21,23 @@ class Oystercard
     @balance = new_balance
   end
 
-
-  def charge(fare = MINIMUM_FARE)
-    new_balance = @balance - fare
-    @balance = new_balance
-  end
-
   def touch_in(station = Station.new)
     @journey_start = station
-  end
-
-  def touch_in
-
     fail 'Insufficient Funds' if @balance < 1
     @journey_log << station
     @in_use = true
   end
 
-
   def touch_out(station = Station.new)
     @journey_end = station
     @journey_log << station
     charge
-
-  def touch_out
-    charge(MINIMUM_FARE)
-
     @in_use = false
   end
 
   private
 
-  def charge(fare)
+  def charge(fare = MINIMUM_FARE)
     new_balance = @balance - fare
     @balance = new_balance
   end
